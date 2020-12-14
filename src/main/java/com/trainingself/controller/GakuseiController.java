b1f44d046edffb1dce28de39c4ebcec1ce517bc2
hatsumi1978 <hatsumi1978@gmail.com>
2020-12-14 16:33:47 +0900

    initial-modify

--- src/main/java/com/trainingself/controller/GakuseiController.java
+++ src/main/java/com/trainingself/controller/GakuseiController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.trainingself.form.MendanForm;
+
 @Controller
 @SessionAttributes(value = { "mendanForm","entryInputForm" })
 public class GakuseiController {
@@ -28,22 +30,22 @@ public class GakuseiController {
 		return "gakusei/gakuseiTop";
 	}
 
+
 	@RequestMapping(value = "/toMenndanInput", method = RequestMethod.GET)
-	public String menndanInput(MendanForm inputForm, Model model) throws ParseException {
+	public String menndanInput(Model model) throws ParseException {
 
 		MendanForm form = new MendanForm();
-		System.out.println(form.getReqdate1());
-		System.out.println(form.getReqtime1());
-
-		model.addAttribute("MendanForm", form);
-//		System.out.println(model.);
-//		System.out.println(model.getReqtime1());
+		model.addAttribute("mendanForm", form);
 
 		return "gakusei/menndanInput";
 	}
 
-	@RequestMapping(value = "/toEntryInput", method = RequestMethod.GET)
-	public String entryInput(Locale locale, Model model) {
+
+
+	@RequestMapping(value = "/toEntryInput", method = RequestMethod.POST)
+	public String entryInput(MendanForm inputForm, Model model) {
+
+		logger.info(inputForm.toString());
 
 		MendanForm form = new MendanForm();
 		form.setRequestDate1(new Date());
@@ -58,6 +60,7 @@ public class GakuseiController {
 		return "gakusei/confirm";
 	}
 
+
 	@RequestMapping(value = "/toFinish", method = RequestMethod.GET)
 	public String finish(Locale locale, Model model) {
 		return "gakusei/finish";
@@ -68,52 +71,7 @@ public class GakuseiController {
 		return "gakusei/gakuseiTop";
 	}
 
-	public class MendanForm{
-		private String reqdate1; //希望日１（日付）
-		private String reqtime1; //希望日１（時刻）
-		private Date requestDate1; //希望日１（日付・時刻（Date））
-		private String reqdate2; //希望日２（日付）
-		private String reqtime2; //希望日２（時刻）
-		private Date requestDate2; //希望日２（日付・時刻（Date））
-
-		public String getReqdate1() {
-			return reqdate1;
-		}
-		public void setReqdate1(String reqdate1) {
-			this.reqdate1 = reqdate1;
-		}
-		public String getReqtime1() {
-			return reqtime1;
-		}
-		public void setReqtime1(String reqtime1) {
-			this.reqtime1 = reqtime1;
-		}
-		public Date getRequestDate1() {
-			return requestDate1;
-		}
-		public void setRequestDate1(Date requestDate1) {
-			this.requestDate1 = requestDate1;
-		}
-		public String getReqdate2() {
-			return reqdate2;
-		}
-		public void setReqdate2(String reqdate2) {
-			this.reqdate2 = reqdate2;
-		}
-		public String getReqtime2() {
-			return reqtime2;
-		}
-		public void setReqtime2(String reqtime2) {
-			this.reqtime2 = reqtime2;
-		}
-		public Date getRequestDate2() {
-			return requestDate2;
-		}
-		public void setRequestDate2(Date requestDate2) {
-			this.requestDate2 = requestDate2;
-		}
 
 
-	}
 
 }
--- src/main/webapp/WEB-INF/views/gakusei/menndanInput.jsp
+++ src/main/webapp/WEB-INF/views/gakusei/menndanInput.jsp
@@ -1,5 +1,6 @@
 <!-- home.jsp -->
 <%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 
 <html>
@@ -12,32 +13,31 @@
 </head>
 
 <body>
-	<h1>menndanInput</h1>
 
-	<h3 class="text">面談の希望日程をお聞かせ下さい (step 1/3)</h3>
+	<form:form action="/toEntryInput" modelAttribute="mendanForm">
 
-	<div class="text">希望日１</div>
-	<form:input id='reqdate1' path="reqdate1" placeholder="日付を選択してください"/>
-	<form:input id='reqtime1' path="reqtime1" placeholder="時刻(00:00)"/>
-	<input type="date">
-	<div>時刻</div>
-	<input type="time">
+		<h1>menndanInput</h1>
 
-	<h3></h3>
+		<h3 class="text">面談の希望日程をお聞かせ下さい (step 1/3)</h3>
 
-	<div class="text">希望日２</div>
-	<input type="date">
-	<div>時刻</div>
-	<input type="time">
+		<div class="text">希望日１</div>
+		<form:input id='reqdate1' path="reqdate1" placeholder="日付を選択してください" />
+		<form:input id='reqtime1' path="reqtime1" placeholder="時刻(00:00)" />
 
-	<h3></h3>
+		<h3></h3>
 
-	<div align="center">
-		<a href="<%=request.getContextPath()%>/toEntryInput"> <input
-			type="button" value=" 次へ進む ">
-		</a>
-	</div>
+		<div class="text">希望日２</div>
+		<form:input id='reqdate2' path="reqdate2" placeholder="日付を選択してください" />
+		<form:input id='reqtime2' path="reqtime2" placeholder="時刻(00:00)" />
 
+		<h3></h3>
+
+		<div align="center">
+			<input type="submit" value="次へ">
+			
+		</div>
+
+	</form:form>
 </body>
 </html>
 
