1a2b67b9afacf68d5f2ef8a2d2e1799bade05887
OST西岡里紗 <Risa.Nishioka@ibm.com>
2020-12-14 19:10:13 +0900

    途中だよ

--- src/main/java/com/trainingself/controller/GakuseiController.java
+++ src/main/java/com/trainingself/controller/GakuseiController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes(value = { "mendanForm","entryInputForm" })
+@SessionAttributes(value = { "mendanForm", "entryInputForm" })
 public class GakuseiController {
 
 	private static final Logger logger = LoggerFactory.getLogger(GakuseiController.class);
@@ -28,47 +28,74 @@ public class GakuseiController {
 		return "gakusei/gakuseiTop";
 	}
 
+	/**
+	 * 学生トップ画面　→　面談日程伺い画面
+	 * http://localhost:8080/mendanInput
+	 *
+	 */
 	@RequestMapping(value = "/toMenndanInput", method = RequestMethod.GET)
-	public String menndanInput(MendanForm inputForm, Model model) throws ParseException {
+	public String menndanInput(Model model) {
 
 		MendanForm form = new MendanForm();
-		System.out.println(form.getReqdate1());
-		System.out.println(form.getReqtime1());
-
-		model.addAttribute("MendanForm", form);
-//		System.out.println(model.);
-//		System.out.println(model.getReqtime1());
+		model.addAttribute("mendanForm", form);
 
 		return "gakusei/menndanInput";
 	}
 
-	@RequestMapping(value = "/toEntryInput", method = RequestMethod.GET)
-	public String entryInput(Locale locale, Model model) {
+	/**
+	 * 面談日程伺い画面　→　エントリーシート入力画面
+	 * http://localhost:8080/entryInput
+	 *
+	 */
+	@RequestMapping(value = "/toEntryInput", method = RequestMethod.POST)
+	public String entryInput(MendanForm inputForm, Model model) throws ParseException {
+
+		System.out.println(inputForm.toString());
+		System.out.println(inputForm.toString());
 
 		MendanForm form = new MendanForm();
 		form.setRequestDate1(new Date());
 
-		model.addAttribute("mendanForm",form);
+		System.out.println(form.getReqdate1());
+		System.out.println(form.getReqtime1());
+
+		model.addAttribute("mendanForm", form);
 
 		return "gakusei/entryInput";
 	}
 
+	/**
+	 * エントリーシート入力画面　→　入力内容確認画面
+	 * http://localhost:8080/confirm
+	 *
+	 */
 	@RequestMapping(value = "/toConfirm", method = RequestMethod.GET)
 	public String confirm(Locale locale, Model model) {
 		return "gakusei/confirm";
 	}
 
+	/**
+	 * 入力内容確認画面　→　登録完了画面
+	 * http://localhost:8080/finish
+	 *
+	 */
 	@RequestMapping(value = "/toFinish", method = RequestMethod.GET)
 	public String finish(Locale locale, Model model) {
 		return "gakusei/finish";
 	}
 
+	/**
+	 * 登録完了画面　→　学生トップ画面
+	 * http://localhost:8080/gakuseiTop
+	 *
+	 */
 	@RequestMapping(value = "/toGakuseiTop", method = RequestMethod.GET)
 	public String returnTop(Locale locale, Model model) {
 		return "gakusei/gakuseiTop";
 	}
 
-	public class MendanForm{
+	public class MendanForm {
+
 		private String reqdate1; //希望日１（日付）
 		private String reqtime1; //希望日１（時刻）
 		private Date requestDate1; //希望日１（日付・時刻（Date））
@@ -76,43 +103,59 @@ public class GakuseiController {
 		private String reqtime2; //希望日２（時刻）
 		private Date requestDate2; //希望日２（日付・時刻（Date））
 
+		@Override
+		public String toString() {
+			return "MendanForm [reqdate1=" + reqdate1 + ", reqtime1=" + reqtime1 + ", requestDate1=" + requestDate1
+					+ ", reqdate2=" + reqdate2 + ", reqtime2=" + reqtime2 + ", requestDate2=" + requestDate2 + "]";
+		}
+
 		public String getReqdate1() {
 			return reqdate1;
 		}
+
 		public void setReqdate1(String reqdate1) {
 			this.reqdate1 = reqdate1;
 		}
+
 		public String getReqtime1() {
 			return reqtime1;
 		}
+
 		public void setReqtime1(String reqtime1) {
 			this.reqtime1 = reqtime1;
 		}
+
 		public Date getRequestDate1() {
 			return requestDate1;
 		}
+
 		public void setRequestDate1(Date requestDate1) {
 			this.requestDate1 = requestDate1;
 		}
+
 		public String getReqdate2() {
 			return reqdate2;
 		}
+
 		public void setReqdate2(String reqdate2) {
 			this.reqdate2 = reqdate2;
 		}
+
 		public String getReqtime2() {
 			return reqtime2;
 		}
+
 		public void setReqtime2(String reqtime2) {
 			this.reqtime2 = reqtime2;
 		}
+
 		public Date getRequestDate2() {
 			return requestDate2;
 		}
+
 		public void setRequestDate2(Date requestDate2) {
 			this.requestDate2 = requestDate2;
 		}
-
 
 	}
 
--- src/main/webapp/WEB-INF/views/gakusei/menndanInput.jsp
+++ src/main/webapp/WEB-INF/views/gakusei/menndanInput.jsp
@@ -1,6 +1,6 @@
 <!-- home.jsp -->
 <%@ page contentType="text/html; charset=UTF-8"%>
-
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 <html>
 <head>
@@ -16,28 +16,26 @@
 
 	<h3 class="text">面談の希望日程をお聞かせ下さい (step 1/3)</h3>
 
-	<div class="text">希望日１</div>
-	<form:input id='reqdate1' path="reqdate1" placeholder="日付を選択してください"/>
-	<form:input id='reqtime1' path="reqtime1" placeholder="時刻(00:00)"/>
-	<input type="date">
-	<div>時刻</div>
-	<input type="time">
+	<form:form action="/toEntryInput" modelAtrribute="mendanForm"
+		autocomplete="off">
 
-	<h3></h3>
+		<div class="text">希望日１</div>
+		<form:input id='reqdate1' path="reqdate1" placeholder="日付を選択してください" />
+		<form:input id='reqtime1' path="reqtime1" placeholder="時刻(00:00)" />
+		<h3></h3>
 
-	<div class="text">希望日２</div>
-	<input type="date">
-	<div>時刻</div>
-	<input type="time">
+		<div class="text">希望日２</div>
+		<input type="date">
+		<div>時刻</div>
+		<input type="time">
 
-	<h3></h3>
+		<h3></h3>
 
-	<div align="center">
-		<a href="<%=request.getContextPath()%>/toEntryInput"> <input
-			type="button" value=" 次へ進む ">
-		</a>
-	</div>
+		<div align="center">
+			<input type="submit" value="次へ">
+		</div>
 
+	</form:form>
 </body>
 </html>
 
